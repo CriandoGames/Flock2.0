19160efb296e4044f23de116afc1646b5f843bd3
Carlos Castro <carloscastrogames@gmail.com>
2018-04-03 17:07:04 -0300

    version 1.0.0.3
    Update Class PlayerScript and PlayerBase

--- Assets/Script/Player/PlayerScript.cs
+++ Assets/Script/Player/PlayerScript.cs
@@ -5,25 +5,16 @@ namespace Script.Player
     public class PlayerScript : PlayerBase, IMotion
     {
           
-       public override void Start()
+         public override void Start()
         {
             base.Start();
         }
-
 
         private void Update()
         {
             Move();
         }
 
-        public void Move()
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                playerAnimator.AnimationFlyer(ForceFlayer);
-                
-                rigidbody2D.AddForce(new Vector2(0,ForceFlayer)); 
-            }
-        }
+
     }
 }
